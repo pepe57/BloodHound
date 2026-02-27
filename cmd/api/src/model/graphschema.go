f597aaa1c476c4d721a03a04ea866347a71f01e1
mistahj67 <26472282+mistahj67@users.noreply.github.com>
2026-02-27 10:51:51 -0700

    feat(OG): Add helper methods to schema_findings BED-7511 (#2410)

--- cmd/api/src/model/graphschema.go
+++ cmd/api/src/model/graphschema.go
@@ -18,6 +18,7 @@ package model
 
 import (
 	"errors"
+	"slices"
 	"time"
 
 	"github.com/specterops/dawgs/graph"
@@ -140,6 +141,7 @@ type GraphSchemaRelationshipKinds []GraphSchemaRelationshipKind
 type GraphSchemaRelationshipKind struct {
 	Serial
 	SchemaExtensionId int32 // indicates which extension this relationship kind belongs to
+	KindId            int32
 	Name              string
 	Description       string
 	IsTraversable     bool // indicates whether the relationship-kind is a traversable path
@@ -183,6 +185,54 @@ type SchemaFinding struct {
 	Name              string
 	DisplayName       string
 	CreatedAt         time.Time
+
+	// This is the kind that the finding is associated with based on the kind_id, it is enriched by db getters
+	Kind graph.Kind `gorm:"-"`
+	// This is the subtypes a finding is associated with, it is enriched by the db getters
+	Subtypes []string `gorm:"-"`
+	// This is the extension a finding is associated with, it is enriched by the db getters
+	Extension GraphSchemaExtension `gorm:"-"`
+}
+
+func (s SchemaFinding) GetType() SchemaFindingType {
+	return s.Type
+}
+
+func (s SchemaFinding) IsType(findingType SchemaFindingType) bool {
+	return s.Type == findingType
+}
+
+func (s SchemaFinding) String() string {
+	return s.Name
+}
+
+func (s SchemaFinding) FindingKind() graph.Kind {
+	return s.Kind
+}
+
+func (s SchemaFinding) GetDisplayName() string {
+	return s.DisplayName
+}
+
+func (s SchemaFinding) GetExtensionName() string {
+	return s.Extension.Name
+}
+
+func (s SchemaFinding) GetSubtypes() []string {
+	return s.Subtypes
+}
+
+func (s SchemaFinding) Is(others ...graph.Kind) bool {
+	for _, other := range others {
+		if other.String() == s.String() {
+			return true
+		}
+	}
+	return false
+}
+
+func (s SchemaFinding) IsSubtype(subtype string) bool {
+	return slices.Contains(s.Subtypes, subtype)
 }
 
 func (SchemaFinding) TableName() string {
